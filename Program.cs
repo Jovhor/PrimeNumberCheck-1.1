bc125a2b17fad7771f5eea90364b7c5534abb2d7
Jovhor <dr.jovhor@mail.ru>
2023-09-20 22:45:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberCheck_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool divideSigh = false;
+
+            Console.WriteLine("Prime number \n");
+
+            Console.WriteLine("Приложение для определения простого числа");
+            Console.WriteLine("Назови число, а я тебе скажу, просто оно или составное.");
+
+            for (; ; )
+            {
+                Console.WriteLine("Введите число:");
+                string userNumberInput = Console.ReadLine();
+                int userNumber = Convert.ToInt32(userNumberInput);
+
+                for (int i = 2; i < userNumber; i++)
+                {
+                    if (userNumber % i == 0)
+                    {
+                        divideSigh = true;
+                        break;
+                    }
+                    else
+                    {
+                        divideSigh = false;
+                    }
+                }
+
+                if (divideSigh)
+                {
+                    Console.WriteLine("Число составное. Ещё разок?");
+                }
+                else
+                {
+                    Console.WriteLine("Число простое. Ещё разок?");
+                }
+            }
+        }
+    }
+}
